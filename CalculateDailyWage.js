c328114f44a160f7071994da7194572c3ed86017
Rajveer188 <veerkajle188@gmail.com>
2025-03-04 18:21:10 +0530

    [Rajveer] Refactor the Code to write a function to get work hours

--- CalculateDailyWage.js
+++ CalculateDailyWage.js
@@ -4,19 +4,21 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
+//method to calculate working hour
+function getWorkingHour(employeeCheck){
+    switch(employeeCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
 let employeeHour = 0;
 employeeCheck = Math.floor(Math.random()*10%3);
-switch(employeeCheck){
-    case IS_PART_TIME:
-        employeeHour = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        employeeHour = FULL_TIME_HOURS;
-        break
-    default:
-        employeeHour = 0;
-}
-
+//call function to calculate working hour
+employeeHour = getWorkingHour(employeeCheck)
 let employeeWage = employeeHour * WAGE_PER_HOUR;
 //print result
 console.log("employee wage ", employeeWage)
