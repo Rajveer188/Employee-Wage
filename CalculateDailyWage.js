337c47e46df1862e50ff22d411ca7bde1448cf5a
Rajveer188 <veerkajle188@gmail.com>
2025-03-04 19:14:12 +0530

    [Rajveer] Add. Store the Daily Wage along with the Total Wage

--- CalculateDailyWage.js
+++ CalculateDailyWage.js
@@ -4,6 +4,10 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
+//method to calculate daily wage
+function calculateDailyWage(employeeHour){
+    return totalEmployeeHour * WAGE_PER_HOUR;
+}
 //method to calculate working hour
 function getWorkingHour(employeeCheck){
     switch(employeeCheck){
@@ -21,12 +25,19 @@ const MAX_HOUR_IN_MONTH = 160;
 let totalEmployeeHour = 0;
 let totalWorkingDay = 0;
 
+//array to store daily wage
+let dailyWageArray = new Array();
+
 while(totalEmployeeHour <= MAX_HOUR_IN_MONTH && totalWorkingDay < NUMBER_OF_DAYS){
     totalWorkingDay++;
     let employeeCheck = Math.floor(Math.random()*10%3);
-    totalEmployeeHour += getWorkingHour(employeeCheck);
+    let employeeHour = getWorkingHour(employeeCheck);
+    totalEmployeeHour += employeeHour;
+    dailyWageArray.push(calculateDailyWage(employeeHour));
 }
 
-let employeeWage = totalEmployeeHour * WAGE_PER_HOUR;
+let employeeWage = calculateDailyWage(totalEmployeeHour)
+
 //print result
+console.log(dailyWageArray);
 console.log("total days ",totalWorkingDay," total hours ", totalEmployeeHour, " employee wage ", employeeWage)
